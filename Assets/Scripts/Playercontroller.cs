abf415cf9061f79c6a0e4bfac25d9423c518fabc
JimmyPV <84383396+JimmyPV@users.noreply.github.com>
2021-09-19 11:53:41 +0200

    Create a game end message

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -8,6 +8,7 @@ public class Playercontroller : MonoBehaviour
 {
     public float speed = 0;
     public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
     private int count;
@@ -20,6 +21,7 @@ public class Playercontroller : MonoBehaviour
         count = 0;
 
         SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -34,6 +36,10 @@ public class Playercontroller : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate()
